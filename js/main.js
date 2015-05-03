c510636138fa236c91f3cd2a087173c63dded145
Joe White <white.joe4@gmail.com>
2015-05-03 18:21:16 +0100

    it bloody works

--- js/main.js
+++ js/main.js
@@ -28,66 +28,83 @@
     this.controls.dynamicDampingFactor = 0.3;
 
     // init sphere rotation state
+    this.radius = 20;
     this.rotation_vector = new THREE.Vector3(0, 0, 0);
     this.rotation_quaternion = new THREE.Quaternion();
 
     this.init_projection();
   };
 
-  Spheye.prototype.key_down = function(event) {
-    switch (event.keyCode) {
-      case 37: this.rotation_vector.z = -1.0; break; // left
-      case 38: this.rotation_vector.x = 1.0; break; // up
-      case 39: this.rotation_vector.z = 1.0; break; // right
-      case 40: this.rotation_vector.x = -1.0; break; // down
-    }
-  };
-
   Spheye.prototype.init_projection = function() {
-    this.radius = 10;
 
-    var box = new THREE.BoxGeometry(this.radius, this.radius, this.radius, this.radius, this.radius, this.radius);
+    var box = new THREE.BoxGeometry(
+        this.radius, this.radius, this.radius, 10, 10, 10);
+
     for (var i = 0; i < box.faces.length; i++) {
        box.faces[i].color.setHex(i/box.faces.length * 0xffffff);
-       box.faces[i].materialIndex = 0;
+       // box.faces[i].materialIndex = 0;
     }
+
     var eyeTexture = new THREE.ImageUtils.loadTexture("img/eye.png");
-	eyeTexture.wrapS = THREE.MirroredRepeatWrapping;
-	eyeTexture.wrapT = THREE.MirroredRepeatWrapping;
-	eyeTexture.repeat.set(8,8);
-	var eyeMat = [new THREE.MeshBasicMaterial({ map: eyeTexture })];
-	var eye = new THREE.MeshFaceMaterial(eyeMat);
+    eyeTexture.wrapS = THREE.MirroredRepeatWrapping;
+    eyeTexture.wrapT = THREE.MirroredRepeatWrapping;
+    eyeTexture.repeat.set(8,8);
+    var eyeMat = [new THREE.MeshBasicMaterial({ map: eyeTexture })];
+    // var eye = new THREE.MeshFaceMaterial(eyeMat);
+    var eye = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
 
-    this.cube = new THREE.Mesh(box, eye);
-    this.cube.position.set(0, this.radius, 0);
-    this.cube.matrixAutoUpdate = true;
-    this.scene.add(this.cube);
+    var cube = new THREE.Mesh(box, eye);
 
-    var projcubegeom = this.proj_cube_sphere(this.cube.geometry.clone(), this.radius);
-    var projcubemat = this.cube.material.clone();
-    projcubemat.transparent = true;
-    projcubemat.opacity = 0.5;
+    // sphere
+    this.sphere = new THREE.Mesh(
+        this.get_sphere_geom_from_cube(cube.geometry.clone(), this.radius),
+        cube.material.clone());
+    this.sphere.position.set(0, this.radius, 0);
+    this.sphere.geometry.mergeVertices();
+    this.scene.add(this.sphere);
 
-    this.projcube = new THREE.Mesh(projcubegeom, projcubemat);
-    this.projcube.position.set(0, this.radius, 0);
-    this.projcube.matrixAutoUpdate = true;
-    this.scene.add(this.projcube);
+    // plane
+    var sphere_geom = this.sphere.geometry.clone();
+    for (i = 0; i < sphere_geom.vertices.length; ++i) {
+      world_vertex = this.sphere.localToWorld(sphere_geom.vertices[i]).clone();
+      world_vertex.y += this.radius;
+    }
 
-    var plaingeom = this.proj_sphere_plain(this.projcube.geometry.clone());
-    var plain = new THREE.Mesh(plaingeom, eye);
-    plain.matrixAutoUpdate = true;
-    plain.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI);
-    this.scene.add(plain);
+    this.plane = new THREE.Mesh(
+        this.get_plane_geom_from_sphere(sphere_geom), eye);
+
+    this.plane.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI);
+    this.scene.add(this.plane);
   };
 
+  Spheye.prototype.get_sphere_geom_from_cube = function(geom, size) {
+    vertices = geom.vertices;
 
-  Spheye.prototype.proj_sphere_plain = function(geom) {
+    for (j = 0; j < vertices.length; ++j) {
+      vertex = vertices[j];
+      x = vertex.x/size*2;  // dont know why *2 but it works
+      y = vertex.y/size*2;
+      z = vertex.z/size*2;
+
+      xx = x*Math.sqrt(1-((y*y)/2)-((z*z)/2)+((y*y*z*z)/3));
+      yy = y*Math.sqrt(1-((z*z)/2)-((x*x)/2)+((x*x*z*z)/3));
+      zz = z*Math.sqrt(1-((x*x)/2)-((y*y)/2)+((y*y*x*x)/3));
+
+      vertex.x = xx*size;
+      vertex.y = yy*size;
+      vertex.z = zz*size;
+      vertices[j] = vertex;
+    }
+    return geom;
+  }
+
+  Spheye.prototype.get_plane_geom_from_sphere = function(geom) {
     vertices = geom.vertices;
 
     for (j = 0; j < vertices.length; ++j) {
       vertex = vertices[j];
       x = vertex.x;
-      y = vertex.y + this.radius;
+      y = vertex.y;
       z = vertex.z;
 
       if (Math.floor(x+0.4999) === 0 && Math.floor(z+0.4999) === 0) {
@@ -109,42 +126,6 @@
     return geom;
   };
 
-  Spheye.prototype.proj_cube_sphere = function(geom, size) {
-    vertices = geom.vertices;
-
-    for (j = 0; j < vertices.length; ++j) {
-      vertex = vertices[j];
-      x = vertex.x/size*2;  // dont know why *2 but it works
-      y = vertex.y/size*2;
-      z = vertex.z/size*2;
-
-      xx = x*Math.sqrt(1-((y*y)/2)-((z*z)/2)+((y*y*z*z)/3));
-      yy = y*Math.sqrt(1-((z*z)/2)-((x*x)/2)+((x*x*z*z)/3));
-      zz = z*Math.sqrt(1-((x*x)/2)-((y*y)/2)+((y*y*x*x)/3));
-
-      vertex.x = xx*size;
-      vertex.y = yy*size;
-      vertex.z = zz*size;
-      vertices[j] = vertex;
-    }
-    return geom;
-  }
-
-  Spheye.prototype.create_sphere = function(radius) {
-    geometry = new THREE.SphereGeometry(radius, 100, 100);
-    for ( var i = 0; i < geometry.faces.length; i ++ ) {
-      geometry.faces[ i ].color.setHex( i/geometry.faces.length * 0xffffff );
-    }
-
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
-    material.transparent = true;
-    material.opacity = 0.5;
-    sphere = new THREE.Mesh(geometry, material);
-    sphere.position.set(0, radius, 0);
-    sphere.geometry.mergeVertices();
-    return sphere;
-  }
-
   Spheye.prototype.rotate = function() {
     var drag = 0.99;
     var minDelta = 0.05;
@@ -155,13 +136,29 @@
     vec.z = (vec.z < -minDelta || vec.z > minDelta) ? vec.z*drag : 0.0;
 
     this.rotation_quaternion.setFromAxisAngle(vec, angle);
-    q = this.cube.quaternion;
+    q = this.sphere.quaternion;
     q.multiplyQuaternions(this.rotation_quaternion, q);
     q.normalize();
 
-    this.cube.setRotationFromQuaternion(q);
-    this.projcube.setRotationFromQuaternion(q);
+    this.sphere.setRotationFromQuaternion(q);
+
+    var sphere_geom = this.sphere.geometry.clone();
+    for (i = 0; i < sphere_geom.vertices.length; ++i) {
+      world_vertex = this.sphere.localToWorld(sphere_geom.vertices[i]).clone();
+      world_vertex.y += this.radius;
+    }
+
+    this.plane.geometry = this.get_plane_geom_from_sphere(sphere_geom);
   }
+
+  Spheye.prototype.key_down = function(event) {
+    switch (event.keyCode) {
+      case 37: this.rotation_vector.z = -1.0; break; // left
+      case 38: this.rotation_vector.x = 1.0; break; // up
+      case 39: this.rotation_vector.z = 1.0; break; // right
+      case 40: this.rotation_vector.x = -1.0; break; // down
+    }
+  };
 
   Spheye.prototype.render = function() {
     this.renderer.render(this.scene, this.camera);
